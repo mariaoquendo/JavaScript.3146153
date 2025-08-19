bf72c7d5276b21963b4883f0af08d5560fe5731c
mariaoquendo <iphonetransmayo2024@gmail.com>
2025-08-19 15:30:16 -0500

    audio_video 4
    terminado

--- 4_audio_video/app.js
+++ 4_audio_video/app.js
@@ -3,6 +3,56 @@ const playVideo = document.querySelector('#play-video');
 const stopVideo = document.querySelector('#stop-video');
 const playStopImg1 = document.querySelector('#play-stop-img1');
 const playStopImg2 = document.querySelector('#play-stop-img2');
+const audio1 = document.querySelector("#audio-1");
+const audio2 = document.querySelector("#audio-2");  
 
 
-console.log(video, playVideo,stopVideo,playStopImg1,playStopImg2)
+
+
+//Función para reproducir video
+function reproducirVideo() {
+  video.play()
+}
+ playVideo.addEventListener('click', reproducirVideo);
+
+
+//Función para detener video
+function pausarVideo() {
+   video.pause()
+}
+stopVideo.addEventListener('click', pausarVideo);
+
+
+function toggleAudio1() {
+
+    if(audio1.paused){
+        audio1.play()
+        playStopImg1.textContent = 'Stop ⏸️';
+    }else{
+        audio1.pause()
+        playStopImg1.textContent = 'Play ▶️';
+    }
+
+}
+
+
+
+playStopImg1.addEventListener('click', toggleAudio1);
+
+
+function toggleAudio2() {
+
+    if(audio2.paused){
+        audio2.play()
+        playStopImg2.textContent = 'Stop ⏸️';
+    }else{
+        audio2.pause()
+        playStopImg2.textContent = 'Play ▶️';
+    }
+
+}
+
+
+
+playStopImg2.addEventListener('click', toggleAudio2);
+
