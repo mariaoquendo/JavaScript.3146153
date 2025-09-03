d5502be25333b6a7c7b39c79b54b4ab81857fe31
mariaoquendo <iphonetransmayo2024@gmail.com>
2025-09-03 15:56:33 -0500

    6_base_de_datos
    terminado

---
+++ 6_base_de_datos_comic/bd.js
@@ -0,0 +1,92 @@
+const comic = {
+
+"nombreComic": "IGNNIS",
+
+"categoria": "Apocalipsis",
+
+"cantidadCap": 5,
+
+"descripcion": "Trata de Sostenibilidad y recursos naturales",
+
+"personajes": ["Aina", "Kiran", "Padre Theo"],
+
+"capítulos":[
+
+{
+
+nombreCap: "Visiones del Abismo",
+
+imgPortada: "assets/imagen1.jpg",
+
+video: "video/cap1.mp4",
+
+descripcion: "Aina tiene una visión aterradora al beber de un manantial. Theo y Kiran reconocen su don y entierran a una criatura deformada, fortaleciendo su vínculo...",
+
+personajes: ["Aina", "Kiran", "Padre Theo"]
+
+},
+
+{
+
+nombreCap: "Advertencia del Fuego",
+
+imgPortada: "assets/imagen2.jpg",
+
+video: "video/cap2.mp4",
+
+descripcion: "En un campamento, el grupo se refugia de una tormenta de fuego. Aina consuela a un niño asustado mientras discuten sobre la responsabilidad humana....",
+
+personajes: ["Aina", "Kiran", "Padre Theo"]
+
+},
+
+{
+
+nombreCap: "La espereza en la ruina",
+
+imgPortada: "assets/imagen3.jpg",
+
+video: "video/cap2.mp4",
+
+descripcion: "Aina planta una semilla en un valle seco, simbolizando esperanza. Kiran se desanima, pero Theo menciona “el corazón verde”. Kiran canta para calmar a las criaturas deformadas...",
+
+personajes: ["Aina", "Kiran", "Padre Theo"]
+
+},
+
+
+{
+
+nombreCap: "Ecos del",
+
+imgPortada: "assets/imagen4.jpg",
+
+video: "video/cap2.mp4",
+
+descripcion: "En un túnel silencioso, Aina tararea buscando conexión. El grupo enfrenta la desorientación, pero encuentran un nuevo lenguaje sagrado...",
+
+personajes: ["Aina", "Kiran", "Padre Theo"]
+
+},
+
+{
+
+
+nombreCap: "El renacer de las corazon verde",
+
+imgPortada: "assets/imagen5.jpg",
+
+video: "video/cap2.mp4",
+
+descripcion: "Aina planta una semilla invisible en una llanura estéril. Al cantar, la tierra tiembla, simbolizando el renacer de la vida...",
+
+personajes: ["Aina", "Kiran", "Padre Theo"]
+
+},
+
+
+]
+
+}
+
+
